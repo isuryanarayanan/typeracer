fa73d5153067ecbb6e9bb6c4960065c6ff798421
devdoodan <devdoodan@gmail.com>
2020-12-30 17:28:34 +0530

    test serv

---
+++ test-serv/precache-manifest.05bf913bb53f3649c5ede3ddd8edde3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a211ee6664a8ead9f330",
+    "url": "/css/app.eded4c10.css"
+  },
+  {
+    "revision": "f7a8100f580222bc5f27",
+    "url": "/css/chunk-vendors.28de1c7f.css"
+  },
+  {
+    "revision": "5e6606c21106e3bdcbb916a80674651e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a211ee6664a8ead9f330",
+    "url": "/js/app.4cc04983.js"
+  },
+  {
+    "revision": "f7a8100f580222bc5f27",
+    "url": "/js/chunk-vendors.96a4d777.js"
+  },
+  {
+    "revision": "355301622870751a2d4c606dd0ac00a6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
