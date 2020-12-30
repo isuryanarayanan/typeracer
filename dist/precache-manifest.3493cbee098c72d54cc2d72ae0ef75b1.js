2096a96fd5ed9593665dae243b0c476052522a00
devdoodan <devdoodan@gmail.com>
2020-12-30 18:00:30 +0530

    serve pusjh

---
+++ dist/precache-manifest.3493cbee098c72d54cc2d72ae0ef75b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5bbb67c6f599650d85e",
+    "url": "/typeracer/css/app.eded4c10.css"
+  },
+  {
+    "revision": "f7a8100f580222bc5f27",
+    "url": "/typeracer/css/chunk-vendors.28de1c7f.css"
+  },
+  {
+    "revision": "ea7104e838a70333ce0222022a7f655f",
+    "url": "/typeracer/index.html"
+  },
+  {
+    "revision": "a5bbb67c6f599650d85e",
+    "url": "/typeracer/js/app.23b2739a.js"
+  },
+  {
+    "revision": "f7a8100f580222bc5f27",
+    "url": "/typeracer/js/chunk-vendors.96a4d777.js"
+  },
+  {
+    "revision": "355301622870751a2d4c606dd0ac00a6",
+    "url": "/typeracer/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/typeracer/robots.txt"
+  }
+]);
