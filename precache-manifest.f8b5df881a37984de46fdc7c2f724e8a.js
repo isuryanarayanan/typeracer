825b2aad65da28932d1c798252ce3fde3bdf785d
devdoodan <devdoodan@gmail.com>
2021-01-06 19:18:44 +0530

    deploy

---
+++ precache-manifest.f8b5df881a37984de46fdc7c2f724e8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b9757c62d43fc1c6cd",
+    "url": "/typeracer/css/app.eded4c10.css"
+  },
+  {
+    "revision": "070fefe64c45f9b728a6",
+    "url": "/typeracer/css/chunk-vendors.1b17669f.css"
+  },
+  {
+    "revision": "292198f930053572b1b60caacdfe3228",
+    "url": "/typeracer/index.html"
+  },
+  {
+    "revision": "28b9757c62d43fc1c6cd",
+    "url": "/typeracer/js/app.4a966a3a.js"
+  },
+  {
+    "revision": "070fefe64c45f9b728a6",
+    "url": "/typeracer/js/chunk-vendors.1f230236.js"
+  },
+  {
+    "revision": "355301622870751a2d4c606dd0ac00a6",
+    "url": "/typeracer/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/typeracer/robots.txt"
+  }
+]);
